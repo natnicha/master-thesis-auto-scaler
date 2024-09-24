a5545bea36dc1171f9221d69aeba5a0aa1977970
Natnicha <nat.rodtong@gmail.com>
2024-09-24 18:24:19 +0200

    feat: complete scaling in-out by calling docker service

--- agent.py
+++ agent.py
@@ -1,9 +1,5 @@
+import logging
 import math
-import os
-from pprint import pprint
-import subprocess
-import time
-import datetime as dt
 
 def reward_calculator(service_info, response_time):
     alpha = 1.0 # weight1
@@ -11,54 +7,15 @@ def reward_calculator(service_info, response_time):
     gamma = 1.5 # weight3
 
     response_time = response_time/1000.0
-    loss = service_info["drops"]/service_info["packets"] if service_info["packets"] != 0 else 1
-    inst_count = service_info["size"]/(service_info["num_types"]*5)
+    # TODO: find drops and total packets to find loss
+    # loss = service_info["drops"]/service_info["packets"] if service_info["packets"] != 0 else 1
+    # inst_count = service_info["size"]/(service_info["num_types"]*5)
+    loss = 0.01
+    inst_count = service_info['instance_num']
 
     reward = -((alpha*math.log(1+response_time)+(beta*math.log(1+loss))+(gamma*math.log(1+inst_count))))
 
-    print("reward value 1 : {}".format(alpha*math.log(1+response_time)))
-    print("reward value 2 : {}".format(beta*math.log(1+loss)))
-    print("reward value 3 : {}".format(gamma*math.log(1+inst_count)))
-
-
+    logging.info(f"reward value 1 : {alpha*math.log(1+response_time)}")
+    logging.info(f"reward value 2 : {beta*math.log(1+loss)}")
+    logging.info(f"reward value 3 : {gamma*math.log(1+inst_count)}")
     return reward
-
-
-# measure_response_time(): send http requests from a source to a destination
-# Input: scaler
-# Output: response time
-def measure_response_time(scaler, name):
-    cnd_path = os.path.dirname(os.path.realpath(__file__))
-
-    dst_ip = get_ip_from_id(scaler.get_monitor_dst_id())
-    src_ip = get_ip_from_id(scaler.get_monitor_src_id())
-
-    command = ("sshpass -p %s ssh -o stricthostkeychecking=no %s@%s ./test_http_e2e.sh %s %s %s %s %s" % (
-        cfg["traffic_controller"]["password"],
-        cfg["traffic_controller"]["username"],
-        cfg["traffic_controller"]["ip"],
-        src_ip,
-        cfg["instance"]["username"],
-        cfg["instance"]["password"],
-        cfg["traffic_controller"]["num_requests"],
-        dst_ip))
-
-    command = command + " | grep 'Time per request' | head -1 | awk '{print $4}'"
-
-    # Wait until web server is running
-    start_time = dt.datetime.now()
-
-    while True:
-        time.sleep(10)
-        response = subprocess.check_output(command, shell=True).strip().decode("utf-8")
-        print(response)
-        if response != "":
-            pprint("[%s] %s" % (scaler.get_scaling_name(), response))
-            f = open("test_monitor-"+name+".txt", "a+", encoding='utf-8')
-            f.write(str(response)+'\n')
-            f.close()
-            print("write done")
-            return float(response)
-        elif (dt.datetime.now() - start_time).seconds > 60 or scaler.get_active_flag() == False:
-            scaler.set_active_flag(False)
-            return -1
--- main.py
+++ main.py
@@ -19,38 +19,21 @@ num_neurons   = 128             # Number of neurons in each hidden layer
 epsilon       = 0.10            # epsilon value of e-greedy algorithm
 required_mem_size = 20          # Minimum number triggering sampling
 print_interval = 20             # Number of iteration to print result during DQN
-max_apisode   = 1000            # Number of episode
+max_apisode   = 10              # Number of episode
 
 scaler_list = []
 sfc_update_flag = True
 
 OKGREEN = '\033[92m'
+WARNING = '\033[93m'
 ENDC = '\033[0m'
 logging.basicConfig(level=logging.INFO, format=f"{OKGREEN}%(levelname)s{ENDC} %(message)s")
 
 def get_containers_info() -> list:
-    # request = requests.get(f"{config['docker']['base_url']}/containers")
-    # return request.json()['containers']
-    return [
-      {
-        "cpu_percent": 0.44,
-        "id": "98d9ad729bd0",
-        "mem_limit_gb": 3.76,
-        "mem_percent": 8.34,
-        "mem_usage_mb": 321.02,
-        "name": "ml.2.ovevrp3"
-      },
-      {
-        "cpu_percent": 0.59,
-        "id": "03626f8424fe",
-        "mem_limit_gb": 3.76,
-        "mem_percent": 8.32,
-        "mem_usage_mb": 320.57,
-        "name": "ml.1.tjg5uvc"
-      }
-    ]
+    request = requests.get(f"{config['docker']['base_url']}/containers")
+    return request.json()['containers']
 
-def calculate_docker_info(containers_info):
+def calculate_service_info(containers_info):
   cpu_percent = 0
   mem_percent = 0
   for container in containers_info:
@@ -77,8 +60,9 @@ def scale_pod(scale: ScalingPod):
     url=f"{config['docker']['base_url']}/scale/{scale}",
     json='',
   )
+  logging.info(f"scaling pod got: status code {request.status_code}")
   if request.status_code == 200:
-    return True
+    return request.json()
   else:
     logging.error(f"scaling pod got: status code {request.status_code}")
     logging.error(f"scaling pod got: {request.json()}")
@@ -91,13 +75,7 @@ def dqn_scaling(scaler: AutoScaler):
 
   # Initial Processing
   start_time = dt.datetime.now() #+ dt.timedelta(hours = 24)
-  containers_info = get_containers_info()
-  docker_info = calculate_docker_info(containers_info)
   epsilon_value = epsilon
-
-  # flavors = ni_mon_api.get_vnf_flavors()
-  # instance_types = get_sfcr_by_id(sfc_info.sfcr_ids[-1]).nf_chain
-  #del instance_types[0] # Flow classifier instance deletion
 
   # Q-networks
   num_states = 2 # Number of states
@@ -108,10 +86,12 @@ def dqn_scaling(scaler: AutoScaler):
   q_target.load_state_dict(q.state_dict())
   
   
-  if scaler.has_dataset == True:
-    q.load_state_dict(torch.load("save_model/"+scaler.get_scaling_name()))
-    q_target.load_state_dict(torch.load("save_model/"+scaler.get_scaling_name()))
-      
+  is_file = os.path.isfile("./save_model/"+scaler.get_scaling_name())
+  # if scaler.has_dataset == True:
+  if is_file:
+    q.load_state_dict(torch.load("./save_model/"+scaler.get_scaling_name()))
+    q_target.load_state_dict(torch.load("./save_model/"+scaler.get_scaling_name()))
+    logging.info("loaded a saved model")
   else:
     logging.info("learning from live data")
 
@@ -126,14 +106,17 @@ def dqn_scaling(scaler: AutoScaler):
   
   # Epsilon_value setting
   # epsilon_value = 0.5
-  
   if scaler.has_dataset == True:
     epsilon_value = 0.11
     logging.info("has dataset")
 
   while scaler.get_active_flag():
+    logging.info(f"{WARNING} ---------- Episode {n_epi} ---------- {ENDC}")
     # Get state and select action
-    state = get_pre_processing_state(docker_info)
+    containers_info = get_containers_info()
+    service_info = calculate_service_info(containers_info)
+    state = get_pre_processing_state(service_info)
+    logging.info(f"state (s): {state}")
     decision = q.sample_action(torch.from_numpy(state).float(), epsilon_value)
     a = decision["action"]
     decision_type = "Policy" if decision["type"] else "R"
@@ -151,64 +134,47 @@ def dqn_scaling(scaler: AutoScaler):
         logging.info("[%s] Maintain! by %s" % (scaler.get_scaling_name(), decision_type))
         scaling_flag = 0
 
-    # For test!!
+    # For testing
     # scaling_flag = 0
     # Scaling in or out
     logging.info(f"Epsilon value : {epsilon_value}")
-    if scaling_flag != 0:
-      num_instances = docker_info['instance_num']
-      logging.info(num_instances)
+    respond = {'time_spent_sec': 0.0}
+
+    num_instances = service_info['instance_num']
+    logging.info(f"current instance: {num_instances}")
+
+    if scaling_flag != 0 and \
+      num_instances+scaling_flag >= config["instance"]["min_number"] and \
+      num_instances+scaling_flag <= config["instance"]["max_number"]:
 
       # Scaling-out
       if scaling_flag > 0:
-        # If possible to deploy new VNF instance
-        if num_instances < config["instance"]["max_number"]:
-          # scale - out command
-          scale_pod(ScalingPod.Out)
-          logging.info('scaling OUT succeeded')
-          # if success; done = True
+        respond = scale_pod(ScalingPod.Out)
+        logging.info(f"scaling OUT succeeded with:{respond['time_spent_sec']}")
+        done = True
 
       # Scaling-in
       elif scaling_flag < 0:
-        # If possible to remove VNF instance
-        if num_instances > config["instance"]["min_number"]:
-          # scale-in
-          scale_pod(ScalingPod.In)
-          logging.info('scaling IN succeeded')
-          # if success; done = True
-
-      # Maintain
-      else:
+        respond = scale_pod(ScalingPod.In)
+        logging.info(f"scaling IN succeeded with:{respond['time_spent_sec']}")
         done = True
+
+    # Maintain
+    else:
+      scaling_flag = 0
+      done = True
         
-    exit(1)
-    
     # Prepare calculating rewards
-    # if scaling_flag == 1 and type_name == "firewall":
-    #     print("waiting time for VNF configuration")
-        
-    # sfc_info = get_sfc_by_name(scaler.get_sfc_name())
-    # vnf_info = get_vnf_info(sfc_info)
-
-    s_prime = state_pre_processor(service_info) # TODO: get pre-process state
-
-    # TODO: measure response time
-    # response_time = 0.0
-    # for rep in range(0,5):
-    #     response_time = max(response_time, measure_response_time(scaler, "DQN"))
-    
-    # response_time = response_time
-    
-    # if response_time < 0:
-    #     break
-
-    # type_instances = get_instances_in_sfc(vnf_info, sfc_info)
-    # type_status = get_type_status(type_instances, flavors)
-
-    r = reward_calculator(service_info, response_time)
+    # Find s' (post state)
+    post_ontainers_info = get_containers_info()
+    post_service_info = calculate_service_info(post_ontainers_info)
+    s_prime = get_pre_processing_state(post_service_info)
+    logging.info(f"post state(s'): {s_prime}")
+    response_time = respond['time_spent_sec']
+    r = reward_calculator(post_service_info, response_time)
 
     done_mask = 1.0 if done else 0.0
-    transition = (s,a,r,s_prime,done_mask)
+    transition = (state,a,r,s_prime,done_mask)
     memory.put(transition)
 
     if memory.size() > required_mem_size:
@@ -238,11 +204,11 @@ def dqn_scaling(scaler: AutoScaler):
   if scaler in scaler_list:
     # delete_monitor(scaler)
     scaler_list.remove(scaler)
-    pprint("[Expire: %s] DQN Scaling" % (scaler.get_scaling_name()))
+    logging.info(f"[Expire: {scaler.get_scaling_name()}] DQN Scaling")
   else:
-    pprint("[Exit: %s] DQN Scaling" % (scaler.get_scaling_name()))
+    logging.info(f"[Exit: {scaler.get_scaling_name()}] DQN Scaling")
 
-  q.save_model("./"+scaler.get_scaling_name())
+  q.save_model("./save_model/"+scaler.get_scaling_name())
 
 
 if __name__ == '__main__':
@@ -253,7 +219,7 @@ if __name__ == '__main__':
         scaling_name='DQN',
         slo=None,
         duration=0,
-        interval=15
+        interval=1
     ), 
     type='dqn')
   )
